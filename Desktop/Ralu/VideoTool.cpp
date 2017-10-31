3fee33137822cf67c27cf7e830a57c9e7dbf09fd
cloud <you@example.com>
2017-10-31 09:49:07 +0200

    socket part-video stream still not working

--- Desktop/Ralu/VideoTool.cpp
+++ Desktop/Ralu/VideoTool.cpp
@@ -5,6 +5,13 @@
 #include "opencv2/highgui/highgui.hpp"
 //#include <opencv2\cv.h>
 #include "opencv2/opencv.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <string.h>
 
 using namespace std;
 using namespace cv;
@@ -36,7 +43,81 @@ const std::string windowName1 = "HSV Image";
 const std::string windowName2 = "Thresholded Image";
 const std::string windowName3 = "After Morphological Operations";
 const std::string trackbarWindowName = "Trackbars";
-
+void miscare(char *ch)
+{
+	int sockfd, portno, n,i;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+   
+   char aux[] = "fslsrsfs";
+   
+   char buffer[256];
+   
+   if (argc < 3) {
+      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      exit(0);
+   }
+	
+   portno = 20232;
+   
+   /* Create a socket point */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+	
+   server = gethostbyname("193.226.12.217");
+   
+   if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+   }
+   
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(portno);
+   
+   /* Now connect to the server */
+   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR connecting");
+      exit(1);
+   }
+   
+   /* Now ask for a message from the user, this message
+      * will be read by server
+   */
+	
+   printf("Please enter the message: ");
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);
+   
+   /* Send message to the server */
+   for(i=0;i<strlen(aux);i++){
+	   char buff[2];
+	   buff[0] = aux[i];
+   n = write(sockfd, buff, strlen(buff));
+   sleep(1);
+   
+   if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }
+   }
+   
+   // /* Now read server response */
+   // bzero(buffer,256);
+   // n = read(sockfd, buffer, 255);
+   
+   // if (n < 0) {
+      // perror("ERROR reading from socket");
+      // exit(1);
+   // }
+	
+   // printf("%s\n",buffer);
+}
 
 void on_mouse(int e, int x, int y, int d, void *ptr)
 {
